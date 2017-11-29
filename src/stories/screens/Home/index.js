4fef2ceec81d237bed527983dfff5161e8d9006f
Anonymous <mukul.vit@gmail.com>
2017-11-29 06:09:44 -0800

    Home screen work in progress

--- src/stories/screens/Home/index.js
+++ src/stories/screens/Home/index.js
@@ -3,6 +3,7 @@ import {
   Container,
   Header,
   Title,
+  Card,
   Content,
   Text,
   Button,
@@ -11,10 +12,11 @@ import {
   Body,
   Right,
   List,
-  ListItem
+  ListItem,
+  CardItem
 } from "native-base";
 
-import { VictoryBar } from "victory-native";
+import {VictoryChart, VictoryLine} from 'victory-native';
 
 import styles from "./styles";
 export interface Props {
@@ -24,9 +26,42 @@ export interface Props {
 export interface State {}
 class Home extends React.Component<Props, State> {
   render() {
+    const items = [
+        {
+          metricName:"Temperature",
+          metricUnit: "degree",
+          value: 36.44,
+          normalValue: 37,
+          threshold: 5,
+          historicalData:[{
+            value: 12,
+            timeStamp: new Date(),
+          }, {
+            value: 13,
+            timeStamp: new Date()
+          }]
+        },
+        {
+          metricName:"Heart Rate",
+          metricUnit: "bpm",
+          value: 78,
+          normalValue: 80,
+          threshold: 3,
+          historicalData: []
+        },
+        {
+          metricName:"Heart Rate",
+          metricUnit: "bpm",
+          value: 78,
+          normalValue: 80,
+          threshold: 3,
+          historicalData: []
+        }
+    ];
+
     return (
       <Container style={styles.container}>
-        <Header>
+        <Header style={styles.header}>
           <Left>
             <Button transparent>
               <Icon
@@ -42,19 +77,49 @@ class Home extends React.Component<Props, State> {
           <Right />
         </Header>
         <Content>
-          <List>
-            {this.props.list.map((item, i) => (
-              <ListItem
-                key={i}
-                onPress={() =>
-                  this.props.navigation.navigate("BlankPage", {
-                    name: { item }
-                  })}
-              >
-              <VictoryBar />
-              </ListItem>
-            ))}
+          <Card style={styles.greetingCard}>
+            <CardItem style={styles.greetingCardItem}>
+              <Body style={styles.greetingCardBody}>
+               <Text style={styles.greetingTitle}>
+                  Welcome Back, Mukul
+               </Text>
+               <Text style={styles.greetingSubtitle}>
+                Things look alright
+               </Text>
+             </Body>
+            </CardItem>
+          </Card>
+
+          <List dataArray={items}
+                      renderRow={(item) =>
+                        <Card style={styles.metricCard}>
+                          <Left style={styles.metricCardTitle}>
+                            <Text style={styles.metricValue}>{item.value}</Text>
+                            <Text style={styles.metricUnit}>{item.metricUnit}</Text>
+                          </Left>
+                          <Body style={styles.metricName}>
+                            <Text style={styles.metricNameText}>{item.metricName}</Text>
+                          </Body>
+                          <Right style={styles.metricChart}>
+                            <VictoryLine
+                              style={{
+                                data: { stroke: "#c43a31" }
+                              }}
+                              height={150}
+                              interpolation="natural"
+                              data={[
+                                { x: 1, y: 2 },
+                                { x: 2, y: 3 },
+                                { x: 3, y: 5 },
+                                { x: 4, y: 4 },
+                                { x: 5, y: 7 }
+                              ]}
+                            />
+                          </Right>
+                        </Card>
+                      }>
           </List>
+
         </Content>
       </Container>
     );
--- src/stories/screens/Home/styles.js
+++ src/stories/screens/Home/styles.js
@@ -2,19 +2,70 @@ import { StyleSheet } from "react-native";
 
 const styles: any = StyleSheet.create({
 	container: {
-		backgroundColor: "#FBFAFA",
 	},
-	row: {
+	greetingCard: {
+		flex: 0,
+		borderWidth: 0,
+		marginTop: 0,
+		marginBottom: 0,
+		marginLeft: 0,
+		marginRight: 0
+	},
+	greetingCardItem: {
+		height: 100,
+		alignItems: "flex-end",
+		flexDirection: "row"
+	},
+	greetingCardBody: {
+		height: 50,
+		flex: 0
+	},
+	greetingTitle: {
+		flexDirection: "column",
 		flex: 1,
+		color: "#7C8B9E",
+		fontWeight: "bold"
+	},
+	greetingSubtitle: {
+		color: "#7C8B9E",
+		fontWeight: "bold"
+	},
+	metricCard: {
+		borderWidth:0,
+		marginTop: 0,
+		marginBottom: 0,
+		marginLeft: 0,
+		marginRight: 0,
+		borderLeftWidth: 0,
+		borderRightWidth: 0,
+		borderTopWidth: 0,
+		borderBottomWidth: 0,
+		flexDirection: "column",
+		alignItems: "center"
+	},
+	metricCardTitle: {
+		flex: 1,
+		alignSelf: "stretch",
 		alignItems: "center",
+		flexDirection: "row",
 	},
-	text: {
-		fontSize: 20,
-		marginBottom: 15,
-		alignItems: "center",
+	metricValue: {
+		flex: 1,
+		textAlign:'right',
+		color: "#3C5073",
+		fontSize: 30,
+		fontWeight: "400"
 	},
-	mt: {
-		marginTop: 18,
+	metricUnit: {
+		flex: 1,
+		textAlign:'left',
+		paddingLeft: 10,
+		fontWeight: "bold",
+		color: "#7C8B9E",
+		fontSize: 20
 	},
+	metricNameText: {
+		color: "#7C8B9E"
+	}
 });
 export default styles;
